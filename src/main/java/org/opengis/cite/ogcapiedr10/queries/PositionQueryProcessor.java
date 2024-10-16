d8e87bb404aa04cabfe0c16edfa13af74dfbaf7a
Benjamin Proß <b.pross@52north.org>
2024-10-16 10:19:14 +0200

    Prevent NPEs

--- src/main/java/org/opengis/cite/ogcapiedr10/queries/PositionQueryProcessor.java
+++ src/main/java/org/opengis/cite/ogcapiedr10/queries/PositionQueryProcessor.java
@@ -63,12 +63,14 @@ public class PositionQueryProcessor extends AbstractProcessor{
                 List<String> crsList = jsonResponse.getList("crs");
 
                 String supportedCRS = null;
-                for (int q = 0; q < crsList.size(); q++) {
-                    if (crsList.get(q).equals("CRS:84") || 
-                    		crsList.get(q).equals("CRS84") || 
-                    		crsList.get(q).equals("EPSG:4326") || 
-                    		crsList.get(q).contains("www.opengis.net/def/crs/OGC/1.3/CRS84")) {
-                        supportedCRS = crsList.get(q);
+                if(crsList != null) {
+                    for (int q = 0; q < crsList.size(); q++) {
+                        if (crsList.get(q).equals("CRS:84") || 
+                                    crsList.get(q).equals("CRS84") || 
+                                    crsList.get(q).equals("EPSG:4326") || 
+                                    crsList.get(q).contains("www.opengis.net/def/crs/OGC/1.3/CRS84")) {
+                            supportedCRS = crsList.get(q);
+                        }
                     }
                 }
                 if (supportedCRS == null) {
@@ -79,14 +81,19 @@ public class PositionQueryProcessor extends AbstractProcessor{
                 HashMap positionQuery = (HashMap) dataQueries.get("position");
                 HashMap link = (HashMap) positionQuery.get("link");
                 HashMap variables = (HashMap) link.get("variables");
-                ArrayList<String> outputFormatList = (ArrayList<String>) variables.get("output_formats");
                 String supportedFormat = null;
-                for (int f = 0; f < outputFormatList.size(); f++) {
-                    if (outputFormatList.get(f).equals("CoverageJSON") || outputFormatList.get(f).contains("CoverageJSON")) {  //preference for CoverageJSON if supported
-                        supportedFormat = outputFormatList.get(f);
-                    }
-                    else if (outputFormatList.get(f).equals("GeoJSON")) {
-                        supportedFormat = outputFormatList.get(f);
+
+                if(variables==null) { //Avoids Nullpointer Exception
+                        sb.append(" The variables element is missing from the collection "+collectionId+" .");
+                } else {
+                    ArrayList<String> outputFormatList = (ArrayList<String>) variables.get("output_formats");
+                    for (int f = 0; f < outputFormatList.size(); f++) {
+                        if (outputFormatList.get(f).equals("CoverageJSON") || outputFormatList.get(f).contains("CoverageJSON")) {  //preference for CoverageJSON if supported
+                            supportedFormat = outputFormatList.get(f);
+                        }
+                        else if (outputFormatList.get(f).equals("GeoJSON")) {
+                            supportedFormat = outputFormatList.get(f);
+                        }
                     }
                 }
 
