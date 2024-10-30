934034be9473b3b74e76b6d2b036346110d2cc11
Benjamin Proß <b.pross@52north.org>
2024-10-30 15:37:54 +0100

    Prevent NoSuchElementException

--- src/main/java/org/opengis/cite/ogcapiedr10/queries/CorridorQueryProcessor.java
+++ src/main/java/org/opengis/cite/ogcapiedr10/queries/CorridorQueryProcessor.java
@@ -65,7 +65,10 @@ public class CorridorQueryProcessor extends AbstractProcessor{
                 Set parameterNamesSet = parameterNames.keySet();
                 Iterator<String> parameterNamesIterator = parameterNamesSet.iterator();
 
-                parameterNamesIterator.hasNext();
+                if(!parameterNamesIterator.hasNext()) { 
+                    sb.append(" The parameter_names element of the collection "+collectionId+" is empty.");
+                    continue;
+                }
                 String sampleParamaterName = parameterNamesIterator.next();
 
                 if(jsonResponse.getList("crs")==null) { //Avoids Nullpointer Exception
