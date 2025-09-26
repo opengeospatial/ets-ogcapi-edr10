53782abd495c5b78ce8d84c9bfdb1d56faada90d
Benjamin Proß <b.pross@52north.org>
2025-09-26 09:11:51 +0200

    Re-activate test, add reference implementation

--- src/test/java/org/opengis/cite/ogcapiedr10/VerifyTestNGController.java
+++ src/test/java/org/opengis/cite/ogcapiedr10/VerifyTestNGController.java
@@ -1,14 +1,13 @@
 package org.opengis.cite.ogcapiedr10;
 
-import org.junit.Ignore;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.w3c.dom.Document;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Source;
 
 /**
  * Verifies the results of executing a test run using the main controller
@@ -21,7 +20,6 @@ public class VerifyTestNGController {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 
-	@Ignore
 	@Test
 	public void testValidateTestRunArgs() throws Exception {
 		// Integration Test
